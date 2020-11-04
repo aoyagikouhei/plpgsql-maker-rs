83013800d92c0650f944afa19af417864abcf840
aoyagikouhei <aoyagi.kouhei@gmail.com>
2020-11-04 09:59:02 +0900

    add params

--- examples/sample/src/main.rs
+++ examples/sample/src/main.rs
@@ -1,5 +1,18 @@
 use plpgsql_maker::hi;
 
+struct InParams {
+    name: String,
+    id: i64,
+}
+
+struct OutParams {
+    name: String,
+    id: i64,
+}
+
+struct Body {
+}
+
 fn main() {
     println!("{}", hi());
 }
